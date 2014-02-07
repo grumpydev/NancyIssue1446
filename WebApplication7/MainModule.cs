a24b765a9f947a429cd7b6483dcfa3d6f2db55e3
Steven Robbins <ste.robbins@gmail.com>
2014-02-07 15:02:45 +0000

    Dumped more debug info

--- WebApplication7/MainModule.cs
+++ WebApplication7/MainModule.cs
@@ -2,11 +2,32 @@
 
 namespace WebApplication7
 {
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using Nancy.Owin;
+
     public class MainModule : NancyModule
     {
         public MainModule()
         {
-            Get["/{name?}"] = _ => _.name;
+            Get["/{name?}"] = _ =>
+                {
+                    var sb = new StringBuilder();
+
+                    sb.AppendFormat("Request Url: {0}\n", Request.Url.ToString());
+                    sb.AppendFormat("Paraneter: {0}\n", _.name);
+
+                    var environment = Context.GetOwinEnvironment();
+
+                    foreach (var envKey in environment.Keys)
+                    {
+                        sb.AppendFormat("{0} : {1}\n", envKey, environment[envKey]);
+                    }
+
+                    return sb.ToString().Replace("\n", "<br/>");
+                };
         }
     }
 }
